d1534e6148a7e108966878a65fbdbfc11f118769
Ankarmin <davidspr.22@gmail.com>
2026-05-17 12:46:43 -0500

    test: isolate app e2e dependencies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,46 @@
+import { INestApplication, Module } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { createRequire } from 'node:module';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+
+const loadModule = createRequire(__filename);
+
+function createEmptyModule() {
+  @Module({})
+  class EmptyModule {}
+
+  return EmptyModule;
+}
 
 describe('AppModule (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication<App> | undefined;
 
   beforeEach(async () => {
+    jest.resetModules();
+    process.env.DATABASE_PUBLIC_URL =
+      'postgresql://postgres:password@localhost:5432/emprendex_test';
+    process.env.JWT_SECRET = 'test-jwt-secret-123';
+
+    jest.doMock('../src/auth/auth.module', () => ({
+      AuthModule: createEmptyModule(),
+    }));
+    jest.doMock('../src/onboarding/onboarding.module', () => ({
+      OnboardingModule: createEmptyModule(),
+    }));
+    jest.doMock('../src/users/users.module', () => ({
+      UsersModule: createEmptyModule(),
+    }));
+    jest.doMock('@nestjs/typeorm', () => ({
+      TypeOrmModule: {
+        forRootAsync: jest.fn(() => createEmptyModule()),
+      },
+    }));
+
+    const appModuleImport = loadModule(
+      './../src/app.module',
+    ) as typeof import('./../src/app.module');
+    const { AppModule } = appModuleImport;
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -21,6 +54,9 @@ describe('AppModule (e2e)', () => {
   });
 
   afterEach(async () => {
-    await app.close();
+    if (app) {
+      await app.close();
+      app = undefined;
+    }
   });
 });
